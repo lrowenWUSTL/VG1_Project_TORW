fa37ccc803be9ee1415170fb96bfe4a3b9e6e8a6
OAHaegt <zexuanwang@wustl.edu>
2023-03-24 21:54:41 -0500

    menu update

--- Assets/Jefferson/MainMenu.cs
+++ Assets/Jefferson/MainMenu.cs
@@ -11,6 +11,11 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene(1);
     }
 
+    public void EndGame()
+    {
+        Application.Quit();
+    }
+
     public void track1()
     {
         SceneManager.LoadScene(2);
